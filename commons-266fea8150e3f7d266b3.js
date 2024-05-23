5495ca2121431fd1e0307da39ea397c3e6383735
theyadavpankaj <theyadavpankaj@gmail.com>
2024-05-23 13:36:51 +0530

    commons-266fea8150e3f7d266b3.js

--- commons-266fea8150e3f7d266b3.js
+++ commons-266fea8150e3f7d266b3.js
@@ -1365,7 +1365,7 @@
                             "stroke-linecap": "round",
                             "stroke-linejoin": "round"
                         }, o.a.createElement("path", {
-                            d: "M23 3a10.9 10.9.0 01-3.14 1.53 4.48 4.48.0 00-7.86 3v1A10.66 10.66.0 013 4s-4 9 5 13a11.64 11.64.0 01-7 2c9 5 20 0 20-11.5a4.5 4.5.0 00-.08-.83A7.72 7.72.0 0023 3z"
+                            d: "M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
                         }))))), o.a.createElement("li", null, o.a.createElement("a", {
                             className: M.a.icon
                         }, o.a.createElement("a", {
